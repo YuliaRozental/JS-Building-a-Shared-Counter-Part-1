47a9c5f1f9209f4033e044cacd5c9057f3f04c0e
Yulia <yuyarozental@gmail.com>
2022-07-06 15:41:52 -0400

    first commit

--- public/index.js
+++ public/index.js
@@ -1,9 +1,13 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
-
-    let countValue = 0;
+    let refreshButton = document.querySelector('refresh-button');
+    
+    let data = await fetch("http://localhost:9001/counter");
+    let response = await data.json();
+    console.log(response);
+    let countValue = response.value;
 
     function increment(){
         countValue++;
@@ -14,9 +18,15 @@ function main(){
         countValue--;
         countContainer.textContent = countValue;
     }
+     
+    function refreshButtonClick() {
+        countContainer.textContent = response.value;
+    }
 
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    refreshButton.addEventListener('click', refreshButtonClick);
     countContainer.textContent = countValue;
+    
 }
-main()
+main();
